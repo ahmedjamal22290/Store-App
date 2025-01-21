1130e3d65d3c69b12093475b754c170fbf2b8e55
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-01-21 14:22:48 +0200

    create app bar for home Page

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  static String id = "HomePage";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Row(
+          children: [
+            Icon(Icons.arrow_back),
+            Spacer(),
+            Text(
+              "New Trend",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Spacer(),
+            Icon(Icons.shopping_cart),
+          ],
+        ),
+      ),
+    );
+  }
+}
