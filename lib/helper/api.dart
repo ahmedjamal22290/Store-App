900595dbaabeb9376be5fde5450d3214e15349a9
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-01-23 14:17:13 +0200

    create put method

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -19,6 +19,13 @@ class Api {
       @required dynamic body,
       @required String? token}) async {
     Map<String, String> headers = {};
+    // {
+//       "title": "test",
+//       "price": "13.5",
+//       "description": "lorem ipsum set",
+//       "image": "https://i.pravatar.cc",
+//       "category": "electronic",
+//     }
     if (token != null) {
       headers.addAll({'Authorization': "Bearer $token"});
     }
@@ -28,15 +35,32 @@ class Api {
       Map<String, dynamic> data = jsonDecode(response.body);
       return data;
     } else {
-      throw Exception("there is an error in status code");
+      throw Exception(
+          "there is an error in status code ${jsonDecode(response.body)}");
+    }
+  }
+
+  Future<dynamic> put(
+      {required String url,
+      required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {
+      "Content-Type": "application/x-www-form-urlencoded"
+    };
+    if (token != null) {
+      headers.addAll({'Authorization': "Bearer $token"});
+    }
+    http.Response response = await http.put(
+      Uri.parse(url),
+      body: body,
+      headers: headers,
+    );
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception(
+          "there is an error in status code ${jsonDecode(response.body)}");
     }
   }
 }
-
-// {
-//       "title": "test",
-//       "price": "13.5",
-//       "description": "lorem ipsum set",
-//       "image": "https://i.pravatar.cc",
-//       "category": "electronic",
-//     }
