19343f2bc4d21cf1ec4496ee6cf713a796fbda5c
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-01-25 12:41:17 +0200

    add Stack wwidget to layer another widgets (the image in card)

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -28,59 +28,66 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 13.0),
-        child: Container(
-          height: 130,
-          width: 220,
-          decoration: BoxDecoration(boxShadow: [
-            BoxShadow(
-                color: Colors.grey.withOpacity(0.3),
-                blurRadius: 40,
-                spreadRadius: -2,
-                offset: Offset(10, 10))
-          ]),
-          child: Card(
-            elevation: 12,
-            color: Colors.white,
-            child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
-              child: Column(
-                // mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "T Shirt ORG",
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Center(
+        child: Stack(
+          children: [
+            Container(
+              height: 130,
+              width: 220,
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    blurRadius: 40,
+                    spreadRadius: 0,
+                    offset: Offset(10, 10))
+              ]),
+              child: Card(
+                elevation: 12,
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        r'$227',
+                        "T Shirt ORG",
                         style: TextStyle(
-                          color: Colors.black,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold,
                           fontSize: 18,
                         ),
                       ),
-                      Icon(
-                        FontAwesomeIcons.solidHeart,
-                        color: Colors.red,
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            r'$227',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                          ),
+                          Icon(
+                            FontAwesomeIcons.solidHeart,
+                            color: Colors.red,
+                          )
+                        ],
                       )
                     ],
-                  )
-                ],
+                  ),
+                ),
               ),
             ),
-          ),
+            Image.asset(
+              'assets/testShirt.png',
+              height: 100,
+            ),
+          ],
         ),
       ),
     );
