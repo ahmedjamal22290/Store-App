c430d379b2117cc15eece897207aa17527e5c413
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-01-26 15:17:17 +0200

    handl keyboard type issue

--- lib/screens/update_product_page.dart
+++ lib/screens/update_product_page.dart
@@ -23,58 +23,60 @@ class UpdateProductPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            customTextField(
-              onChange: (data) {
-                productName = data;
-              },
-              labelText: 'Product Name',
-            ),
-            SizedBox(height: 18),
-            customTextField(
-              onChange: (data) {
-                description = data;
-              },
-              labelText: 'Description',
-            ),
-            SizedBox(height: 18),
-            customTextField(
-              onChange: (data) {
-                price = data as double;
-              },
-              labelText: 'Price',
-            ),
-            SizedBox(height: 18),
-            customTextField(
-              onChange: (data) {
-                image = data;
-              },
-              labelText: 'Image',
-            ),
-            SizedBox(height: 80),
-            GestureDetector(
-              onTap: () {},
-              child: Container(
-                height: 50,
-                width: 90,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  color: Color(0xff344E41),
-                ),
-                child: Center(
-                    child: Text(
-                  'Update',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 21,
-                      fontWeight: FontWeight.bold),
-                )),
+        child: Center(
+          child: ListView(
+            children: [
+              customTextField(
+                onChange: (data) {
+                  productName = data;
+                },
+                labelText: 'Product Name',
               ),
-            ),
-            SizedBox(height: 80),
-          ],
+              SizedBox(height: 18),
+              customTextField(
+                onChange: (data) {
+                  description = data;
+                },
+                labelText: 'Description',
+              ),
+              SizedBox(height: 18),
+              customTextField(
+                keyboardType: TextInputType.number,
+                onChange: (data) {
+                  price = double.parse(data);
+                },
+                labelText: 'Price',
+              ),
+              SizedBox(height: 18),
+              customTextField(
+                onChange: (data) {
+                  image = data;
+                },
+                labelText: 'Image',
+              ),
+              SizedBox(height: 80),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  height: 50,
+                  width: 90,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Color(0xff344E41),
+                  ),
+                  child: Center(
+                      child: Text(
+                    'Update',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 21,
+                        fontWeight: FontWeight.bold),
+                  )),
+                ),
+              ),
+              SizedBox(height: 80),
+            ],
+          ),
         ),
       ),
     );
