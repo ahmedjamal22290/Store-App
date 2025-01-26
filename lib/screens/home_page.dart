9a67ef3ec0356cfcffa1a78e1fdc5819bc8c8e65
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-01-26 13:02:37 +0200

    adding loading process and future builder to get data

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:k/custom/product_card_widget.dart';
 import 'package:k/models/productModel.dart';
+import 'package:k/service/get_all_products.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -32,26 +34,42 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.only(right: 10.0, left: 10, top: 85),
-        child: GridView.builder(
-            clipBehavior: Clip.none,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 1.4,
-                mainAxisSpacing: 85,
-                crossAxisSpacing: 8),
-            itemBuilder: (context, index) {
-              return productCardWidget(
-                productInfo: productModel(
-                    category: "category",
-                    description: 'description',
-                    id: 7,
-                    image:
-                        'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
-                    price: 223,
-                    title: 'Mens Casual Premium Slim Fit T-Shirts ',
-                    rating: ratingModel(count: 4, rate: 5)),
+        child: FutureBuilder<List<productModel>>(
+          future: GetAllProducts().getProducts(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return GridView.builder(
+                  itemCount: snapshot.data!.length,
+                  clipBehavior: Clip.none,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 1.4,
+                      mainAxisSpacing: 85,
+                      crossAxisSpacing: 8),
+                  itemBuilder: (context, index) {
+                    return productCardWidget(
+                      productInfo: productModel(
+                          category: "category",
+                          description: 'description',
+                          id: 7,
+                          image:
+                              'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+                          price: 223,
+                          title: 'Mens Casual Premium Slim Fit T-Shirts ',
+                          rating: ratingModel(count: 4, rate: 5)),
+                    );
+                  });
+            } else {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 300.0),
+                child: Center(
+                  child: LoadingAnimationWidget.threeRotatingDots(
+                      color: Colors.red, size: 115),
+                ),
               );
-            }),
+            }
+          },
+        ),
       ),
     );
   }
--- lib/service/get_all_products.dart
+++ lib/service/get_all_products.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 
 import 'package:k/helper/api.dart';
 import 'package:k/models/productModel.dart';
@@ -5,12 +6,16 @@ import 'package:k/models/productModel.dart';
 class GetAllProducts {
   String baseUrl = "https://fakestoreapi.com/";
   Future<List<productModel>> getProducts() async {
+    log('before await');
     List<dynamic> data = await Api().get('${baseUrl}products');
+    log('after await');
 
     List<productModel> ListOfProducts = [];
     for (int i = 0; i < data.length; i++) {
+      log(i.toString());
       ListOfProducts.add(productModel.fromJson(data[i]));
     }
+
     return ListOfProducts;
   }
 }
