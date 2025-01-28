99dd4d7131ee21ef9d53806035681d979acf3dde
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-01-28 14:30:21 +0200

    edit cubit

--- lib/cubit/isfav_cubit/isfav_cubit.dart
+++ lib/cubit/isfav_cubit/isfav_cubit.dart
@@ -1,14 +1,16 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:k/cubit/isfav_cubit/isFav_states.dart';
 
-class IsFavCubit extends Cubit<IsFavStates> {
-  IsFavCubit() : super(notFav());
-  bool isRed = false;
-  void IsRed() {
-    if (isRed) {
-      emit(IsFav());
-    } else {
-      emit(notFav());
-    }
+class IsFavCubit extends Cubit<IsFavState> {
+  IsFavCubit() : super(IsFavInitial());
+
+  final Map<String, bool> _favorites = {};
+
+  void toggleFavorite(String productId, bool isFav) {
+    _favorites[productId] = isFav;
+    emit(IsFavUpdated(_favorites));
+  }
+
+  bool isFavorite(String productId) {
+    return _favorites[productId] ?? false;
   }
 }
